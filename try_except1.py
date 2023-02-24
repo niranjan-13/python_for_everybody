341569da5b3d5bdb8ae25a5cb3ab62dc86c421cf
Niranjan <pniranjan1303@gmail.com>
2023-02-24 16:34:21 +0530

    example for try except

---
+++ try_except1.py
@@ -0,0 +1,10 @@
+#Converting String to Integer Using try and except 
+val = input("Enter:")
+try:
+	new_val=int(val)
+except:
+	new_val=-1
+if(new_val == -1):
+	print("You Entered Incorrect String, Try agian!")
+else:
+	print(new_val)
