bdeab194b9eaf4f2457b80833b7d2cafc8edd2ce
Niranjan <pniranjan1303@gmail.com>
2023-03-08 15:46:51 +0530

    Added new change
    It prints the most repeatd word and no of times it was repeated

--- repeat_words.py
+++ repeat_words.py
@@ -8,3 +8,13 @@ with open(name) as fhand:
 			mydict[word]=mydict.get(word,0)+1
 	for i in mydict.keys():
 		print(i,mydict[i])
+print("The Most repeated word is:")
+maxWord = None
+maxTimes = 0
+for i,j in mydict.items():
+	if j>maxTimes:
+		maxTimes = j
+		maxWord = i
+	
+print("Most repeated word is :",maxWord)
+print("It is repeated ",maxTimes,"times")
